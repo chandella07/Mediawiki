b4d41efc4ce0ba9abb88be6ca49307b34494a2d8
Deepak Chandella <deepchandila07@gmail.com>
2023-03-06 22:57:59 +0530

    added newline char in script

--- wrapper.sh
+++ wrapper.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Prompt user for username of VM
-read -s -p "Enter VM Username: " ADMIN_USERNAME
+read -s -p "Enter VM Username: "$'\n' ADMIN_USERNAME
 
 # Prompt user for password of VM User
-read -s -p "Enter VM user's password: " ADMIN_PASSWORD
+read -s -p "Enter VM user's password: "$'\n' ADMIN_PASSWORD
 
 # Prompt user for mysql root password
-read -s -p "Enter mysql root password: " MYSQL_ROOT_PASSWORD
+read -s -p "Enter mysql root password: "$'\n' MYSQL_ROOT_PASSWORD
 
 # Prompt user for mysql user password
-read -s -p "Enter mysql user password: " MYSQL_USER_PASSWORD
+read -s -p "Enter mysql user password: "$'\n' MYSQL_USER_PASSWORD
 
 # Setting ENV Vars
 export TF_VAR_admin_user=$ADMIN_USERNAME
@@ -19,7 +19,7 @@ export mysql_root_pwrd=$MYSQL_ROOT_PASSWORD
 export mysql_user_pwrd=$MYSQL_USER_PASSWORD
 
 # moving to terraform dir
-cd mediawiki/mediawiki-terraform
+cd mediawiki-terraform
 
 
 # Executing terraform for creating infrastructure
@@ -33,7 +33,6 @@ export VM_IP=$(terraform output azurerm_linux_virtual_machine.tf_vm1.public_ip_a
 
 # moving to ansible dir
 cd ../mediawiki-ansible/
-
 
 # executing ansible role
 ansible-playbook -i $VM_IP, test-playbook.yml -e "ansible_user=$TF_VAR_admin_user" -e "ansible_password=$TF_VAR_admin_passwd" -e "ansible_sudo_pass=$TF_VAR_admin_passwd"
